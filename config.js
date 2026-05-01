3e701efaea710f767b5ec9bd0f23dc5fd781e856
Shital-P276 <shital.kitcoek@gmail.com>
2026-05-01 07:58:47 +0530

    test

--- config.js
+++ config.js
@@ -10,10 +10,7 @@ const CONFIG = {
   AUTH_BASE_URL: 'https://identitytoolkit.googleapis.com/v1',
   TOKEN_URL: 'https://securetoken.googleapis.com/v1',
   
-  // Groq API Config (for text analysis)
-  // GROK_API_KEY: 'gsk_UR5vAUHaxFwAbCLCsSLkWGdyb3FYvAaThGVQxv89QgW17y00ECD',
-  // GROK_API_URL: 'https://api.groq.com/openai/v1/chat/completions',
-  // GROK_MODEL: 'llama-3.1-8b-instant'
+  
 };
 
 // Export for ES modules or assign to window
