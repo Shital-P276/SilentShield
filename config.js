a4146e28f7e8a416dcdfa491a2e5e4507d3e2d02
Shital-P276 <shital.kitcoek@gmail.com>
2026-05-01 07:58:09 +0530

    test

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   TOKEN_URL: 'https://securetoken.googleapis.com/v1',
   
   // Groq API Config (for text analysis)
-  // GROK_API_KEY: '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',
+  // GROK_API_KEY: 'gsk_UR5vAUHaxFwAbCLCsSLkWGdyb3FYvAaThGVQxv89QgW17y00ECD',
   // GROK_API_URL: 'https://api.groq.com/openai/v1/chat/completions',
   // GROK_MODEL: 'llama-3.1-8b-instant'
 };
